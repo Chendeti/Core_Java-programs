4547b927e7d40d97ed6976ec32dd65ad3f8ac0f5
Chendeti <104186390+Chendeti@users.noreply.github.com>
2022-09-15 14:51:32 +0530

    Add files via upload

---
+++ Core_Java/src/oopsConcepts/Honda.java
@@ -0,0 +1,22 @@
+package oopsConcepts;
+// abstract class 
+
+abstract class Bike{
+	
+	abstract void run();
+	
+}
+
+public class Honda extends Bike {
+	
+	void run() {System.out.println("running safely");}
+
+	public static void main(String[] args) {
+  
+		Bike obj = new Honda();
+		obj.run();
+		
+
+	}
+
+}
